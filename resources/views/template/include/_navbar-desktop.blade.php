71d62f277beb9e3d89e195091ceb5413f1839cdf
Rayhan Abduhuda <rayrayhan920@gmail.com>
2025-11-04 16:53:21 +0700

    fix navbar

--- resources/views/template/include/_navbar-desktop.blade.php
+++ resources/views/template/include/_navbar-desktop.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-dark px-3" style="background: #C49A6C;">
+<nav class="navbar navbar-dark px-3" style="background: #C49A6C; position: fixed; top: 0; width: 100%; z-index: 1000;">
     <a class="navbar-brand" href="{{ route('dashboard.index') }}">
         <img src="{{ asset('img/logo-anda.png') }}" alt="Hotel Logo" class="brand-logo-img-only">
     </a>
@@ -6,8 +6,13 @@
 
 <style>
     .brand-logo-img-only {
-    max-height: 88px;  
-    width: auto;       
-    display: block;
-}
+        max-height: 88px;  
+        width: auto;       
+        display: block;
+    }
+
+    /* Tambahkan padding-top pada konten agar tidak tertutup navbar */
+    body {
+        padding-top: 88px; /* sesuaikan dengan tinggi navbar */
+    }
 </style>
